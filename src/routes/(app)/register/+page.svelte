93c81831d4efb8afeaa61f26adbddb1c81bde7e8
jellyfish <tjdc@myyahoo.com>
2023-08-19 05:34:39 +0800

    Add working toasts

--- src/routes/(app)/register/+page.svelte
+++ src/routes/(app)/register/+page.svelte
@@ -1,40 +1,73 @@
 <script lang="ts">
+    import Toasts from "$lib/toasts/Toasts.svelte";
+    import { addToast } from "$lib/toasts/store";
+    //toast constants
+    let message:String;
+    let type:String;
+    let dismissible:boolean = true;
+    let timeout:number = 2500;
+
+
     let accountLog:any = [];
-    var data:any = []
+    var data:any = [];
     const registerFunction = (event:Event) => {
         if (event && event.target instanceof HTMLFormElement) {
             const formData = new FormData(event.target)
             for (let field of formData) {
-                const [key, value] = field
-                data[key] = value
+                const [key, value] = field;
+                data[key] = value;
             }
 
-            let emailCheck = true
-            let passwordCheck = true
+            let emailCheck = true;
+            let passwordCheck = true;
             
             for (let i = 0; i < accountLog.length && emailCheck == true; i++) {
                 if (data.email == accountLog[i].email) {
-                    alert("Email taken")
-                    emailCheck = false
+                    message = "Email taken";
+                    type = "error";
+                    addToast({message, type, dismissible, timeout});
+                    emailCheck = false;
                 }
             }
             // Password check
             if (data.password !== data.repeatPassword) {
-                alert("Passwords don't match")
+                message = "Passwords don't match";
+                type = "error";
+                addToast({message, type, dismissible, timeout});
                 passwordCheck = false
             }
             if (emailCheck && passwordCheck) {
-                console.log("Passed all validation checks")
+                message = `Hello ${data.firstName}, you have ${data.role.toLowerCase()} access.`;
+                type = "success";
+                addToast({message, type, dismissible, timeout});
                 accountLog.push(data)
             }
         }
         data = [] // This fixes a bug in which all the contents of dataLog gets replaced with the latest value of data.
         console.log(accountLog)
     }
+
+    // let passwordState:boolean = false;
+    // let passwordStateValue = "password";
+    // let repeatPasswordState:boolean = false;
+
+    // const changePasswordVisibility = () => {
+    //     let output:string;
+    //     if (!passwordState) passwordStateValue = "password";
+    //     else passwordStateValue = "text";
+    //     passwordState = !passwordState;
+    // }
+    // const changeRepeatPasswordVisibility = () => {
+    //     let output:string;
+    //     if (!repeatPasswordState) output = "password";
+    //     else output = "text";
+    //     repeatPasswordState = !repeatPasswordState;
+    //     return output;
+    // }
 </script>
 
 <h1>Register</h1>
-
+<button>Sample</button>
 <form on:submit|preventDefault = {registerFunction}>
     <div class="formItem" id="firstnameDiv">
         <label for="firstName">First Name</label>
@@ -66,7 +99,7 @@
         <label for="user">User</label>
         <input type="radio" name="role" id="admin" class="registrationData" value="ADMIN">
         <label for="admin">Admin</label>
-        <input type="radio" name="role" id="guest" class="registrationData" value="GUEST">
+        <input type="radio" name="role" id="guest" class="registrationData" value="GUEST" required>
         <label for="guest">Guest</label>
     </div>
     
